9cdf7877a4aefa038dc835665ce2df1062d1fa07
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2018-10-16 23:47:01 +0300

    styled product item component

--- src/components/ProductsItem/index.js
+++ src/components/ProductsItem/index.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import { ListItem, Left, Body, Thumbnail } from 'native-base';
 import styled from 'styled-components';
+import { TouchableOpacity, View } from 'react-native';
 import { getProductImageURL } from '../../utils/misc';
 
 const TextStyled = styled.Text``;
 
 const ImageStyled = styled.Image`
-  width: 70;
-  height: 70;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
 `;
 
-const ViewStyled = styled.View`
+const ItemContainerStyled = styled.View`
+  border-bottom-width: 0.5px;
+  border-color: #cccccc;
+  background-color: white;
+  flex: 1;
+  flex-direction: row;
+  height: 90px;
+`;
 
+const  ItemImageContainerStyled = styled.View`
+  border-right-width: 0.5px;
+  border-color: #cccccc;
+`;
+
+const ItemContentContainerStyled = styled.View`
+  margin: 10px;
 `;
 
 export default class Item extends Component {
@@ -24,17 +40,23 @@ export default class Item extends Component {
     const { title, id, size } = this.props;
 
     return (
-      <ListItem button onPress={this.handleClick}>
-        <Thumbnail square large source={{uri: getProductImageURL({ id, size })}} />
-        <ViewStyled style={{flex: 1}}>
-          <ViewStyled style={{flex: 1, backgroundColor: 'powderblue'}}>
-            <TextStyled>
-              {title}
-            </TextStyled>
-          </ViewStyled>
-          <ViewStyled style={{flex: 2, backgroundColor: 'skyblue'}} />
-        </ViewStyled>
-      </ListItem>
+      <TouchableOpacity onPress={this.handleClick}>
+        <ItemContainerStyled>
+          <ItemImageContainerStyled style={{flex: 1 }}>
+            <ImageStyled source={{uri: getProductImageURL({ id, size })}} resizeMode="contain" />
+          </ItemImageContainerStyled>
+          <ItemContentContainerStyled style={{flex: 2}}>
+            <View style={{flex: 1}}>
+              <TextStyled>
+                {title}
+              </TextStyled>
+            </View>
+            <View style={{flex: 1}}>
+              <TextStyled>219 р.</TextStyled>
+            </View>
+          </ItemContentContainerStyled>
+        </ItemContainerStyled>
+      </TouchableOpacity>
     );
   }
 }
